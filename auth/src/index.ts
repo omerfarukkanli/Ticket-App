10698ac482aadc25700a05748edc7439f5f9e932
Omer Faruk Kanlı <kanlomerfaruk@gmail.com>
2025-03-13 01:18:43 +0300

    custom error middleware added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,35 @@
+import 'express-async-errors';
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.get('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    errorHandler(err, req, res, next);
+  }
+);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
