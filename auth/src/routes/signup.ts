cafbc4b2b9eb526bc6c5b91920aef0834dac773e
Omer Faruk Kanlı <kanlomerfaruk@gmail.com>
2025-03-15 14:29:18 +0300

    signup service added and user mongo model created

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
+import { CustomError } from '../errors/custom-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -14,18 +16,19 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters'),
   ],
-  (req: Request, res: Response): any => {
+  async (req: Request, res: Response) => {
     const { email, password } = req.body;
+
     const errors = validationResult(req);
+    if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
 
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
+    const user = await User.findOne({ email });
+    if (user) throw new BadRequestError('Email in use');
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    const newUser = await User.build({ email, password });
+    await newUser.save();
 
-    res.send({});
+    res.status(201).send(newUser);
   }
 );
 
